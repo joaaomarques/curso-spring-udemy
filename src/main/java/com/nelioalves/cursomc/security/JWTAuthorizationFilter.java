6081b7601aa6ba912eeaebbbe2f9d4d273248b25
João Marques <jvmarkes@gmail.com>
2021-10-18 22:29:05 -0300

    Correção do erro do endpoint Esqueci a Senha

--- src/main/java/com/nelioalves/cursomc/security/JWTAuthorizationFilter.java
+++ src/main/java/com/nelioalves/cursomc/security/JWTAuthorizationFilter.java
@@ -38,8 +38,8 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 			if (auth != null) {
 				SecurityContextHolder.getContext().setAuthentication(auth);
 			}
-			chain.doFilter(request, response);
 		}
+		chain.doFilter(request, response);
 
 	}
 
